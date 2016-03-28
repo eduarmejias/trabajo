b62681590aa2bf9a3fe203ebd41ef1c66a8bc7b6
alex <eduar.mejias26@gmail.com>
2016-03-28 15:44:34 -0430

    primer commit

---
+++ common/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>HOME PAGE | Mobile Version</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
+
+    
+  </head>
+  
+  <body>
+
+<div class="container" id="container">   
+
+  <div class="row">
+
+          <div class="col-xs-10 col-md-offset-1" id="header"> <!--header-->
+
+                                               <div class="row">
+
+                                                            <div class="col-md-3" id="logo" >
+                                                                <IMG SRC="images/logo.png" WIDTH=100% HEIGHT=250 ALT="" id="imglogo"></IMG>
+                                                            </div>
+                                                             <div class="col-md-8 hidden-xs hidden-sm" id="hueco"> </div>  
+
+
+                                                            <div class="col-md-9" id="navheader" >
+                                                               
+                                                              <div class="col-md-8 " id="nav" >
+
+                                                                 <a href="#" id="btnheader">Home</a>
+                                                                <a href="#" id="btnheader">Menu</a>
+                                                                <a href="#" id="btnheader">Order  online</a>
+                                                                <a href="#" class="hidden-xs hidden-sm" id="btnheader">Info</a>
+                                                                <a href="#" class="hidden-xs hidden-sm hidden-md" id="btnheader">Photos</a>
+                                                                <a href="#" id="btnheader">Contact</a>
+                                                                <a href="#" id="btnheader" class="hidden-md hidden-lg">Francais</a> 
+
+                                                              </div>
+
+                                                              <div class="col-md-4 hidden-xs hidden-sm" id="navlogin">
+                                                                 <strong><a href="#" >Login</a></strong>
+                                                                  <span class="hidden-xs">|</span>
+                                                                <a href="#">Create account</a>
+                                                              </div>
+
+                                                            </div>
+                                                            
+
+                                                            <div class="col-md-9 hidden-xs hidden-sm" id="nav2" >
+                                                                
+                                                               <div class="col-md-2 " >
+                                                                <button type="button" class="btn btn-default" id="btnorder">order online!</button>
+                                                               </div>
+
+
+                                                               <div class="col-md-7 col-md-offset-3 hidden-xs hidden-sm"  id="postal">
+                                                                  <div class="col-md-12" id="area">
+                                                                      <span ><strong>Delivery area codes:</strong> H1K H1J H1L H1M H1N H1K H1J ... </span>
+                                                                  </div>
+                                                               </div>
+
+                                                            </div>
+                                                 </div>
+
+          </div> <!--header-->
